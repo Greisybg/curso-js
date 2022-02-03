094d3d047be6141bd0f071481a18085597d2bbb2
Greisybg <95661929+Greisybg@users.noreply.github.com>
2022-02-02 23:38:30 -0300

    demostrar el uso del while

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+    let numero = parseInt(prompt("Ingresa un número del 0 al 10"));
+    
+
+    while (numero <= 10){ 
+
+    alert ("El número " + (numero +1) + " es mayor al número que ingresaste, te gané!");
+    numero = parseInt(prompt("Ingrese un número del 0 al 10"));
+    
+   }
+
+   alert("Ingresaste un numero invalido, fin del juego");
+
+
